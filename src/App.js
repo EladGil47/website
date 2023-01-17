cd9235054b988d3d23962d01d7ca9e850e423018
EladGil47 <87933794+EladGil47@users.noreply.github.com>
2023-01-17 15:08:42 +0200

    all_works

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
 import PageWrapper from './components/PageWrapper';
 import Home from './components/Pages/Home';
+import { BrowserRouter as Router, Route, Link  } from "react-router-dom";
 
 
 
 class App extends Component {
   render(){ 
   return (
-    <PageWrapper>
+    <Router>
       <Home />  
+    <PageWrapper>
+      <Route 
+      path="/"/>
     </PageWrapper>
+
+
+    </Router>
+
   );
 }
 }
