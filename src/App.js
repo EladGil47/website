1c1c2dc737b711e436b96c38b17450af800e9de8
EladGil47 <87933794+EladGil47@users.noreply.github.com>
2023-01-18 19:38:26 +0200

    add_Contact

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import PageWrapper from './components/PageWrapper';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-// import Header from './components/Common/Header';
 
 // Pages
 import Home from './components/Pages/Home';
 import About from './components/Pages/About';
+import Contact from "./components/Pages/Contact";
 
 
 class App extends Component {
@@ -17,12 +17,18 @@ class App extends Component {
             exact={true}
             path="/"
             component={Home}
-            /> 
+          />
 
           <Route
             path="/about"
             component={About}
-            /> 
+          />
+
+          <Route
+            path="/contact"
+            component={Contact}
+          />
+          
         </PageWrapper>
       </Router>
     );
@@ -30,39 +36,3 @@ class App extends Component {
 }
 
 export default App;
-
-   // <Router>
-
-
-      //   <PageWrapper>
-      //      <Route
-      //         exact={true}
-      //         path="/about"
-      //         component={About}
-      //       />
-
-
-      //     {/* <Route
-      //       exact={true}
-      //       path="/"
-      //       component={Home}
-      //       /> 
-
-      //     <Route
-      //       exact={true}
-      //       path="/about"
-      //       component={About}
-      //       />  */}
-
-      //   </PageWrapper>
-
-
-      // </Router>
-
-
-
-      // <PageWrapper>
-      // <Routes>
-      //   <Route path="/about/*" element={<About />} />
-      // </Routes>
-      // </PageWrapper>
